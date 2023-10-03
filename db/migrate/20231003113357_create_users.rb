67d1abeab19f328a7453727068811997f9b5b8d2
ZuhaaRana <zuhaarana@gmail.com>
2023-10-03 17:25:36 +0500

    Validations to User Model

--- db/migrate/20231003113357_create_users.rb
+++ db/migrate/20231003113357_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :first_name, null: false,
-      t.string :last_name
-      t.string :email
-      t.string :password_digest
-      t.string :phone_no
-      t.string :address
-      t.string :user_type
-      t.integer :user_type_id
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :phone_no, null: false
+      t.string :address, null: false
+      t.string :user_type, null: false
+      t.integer :user_type_id, null: false
 
       t.timestamps
     end
