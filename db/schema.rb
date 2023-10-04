4b2bda7b482f34800ec2a9683fe31ee55d653642
ZuhaaRana <zuhaarana@gmail.com>
2023-10-04 12:09:59 +0500

    Deals Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_070635) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_070940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_070635) do
     t.string "name"
     t.integer "quantity"
     t.string "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "deals", force: :cascade do |t|
+    t.string "name"
+    t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
