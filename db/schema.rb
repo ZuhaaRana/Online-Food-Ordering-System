acc111272618e0f800a2a3351096353cf4e9585a
ZuhaaRana <zuhaarana@gmail.com>
2023-10-04 13:12:23 +0500

    Payment Method Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_080902) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_081157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_080902) do
 
   create_table "orders", force: :cascade do |t|
     t.date "order_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payment_methods", force: :cascade do |t|
+    t.string "payment_method"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
