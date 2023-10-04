d76fcfb8caf00b720d3c6c799128f454a82cbb58
ZuhaaRana <zuhaarana@gmail.com>
2023-10-04 15:52:30 +0500

    Admin Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_105003) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_105207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_105003) do
     t.string "name"
     t.integer "quantity"
     t.string "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "admins", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
