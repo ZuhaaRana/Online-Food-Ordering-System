38c6e74917eee419d02b4f36f8b709af2d9ab9a5
ZuhaaRana <zuhaarana@gmail.com>
2023-10-04 11:41:03 +0500

    Restaurant Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_03_113357) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_063947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "restaurant_name"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
