06eb260ead084a5ab396f0cb05dee6f1c8341707
ZuhaaRana <zuhaarana@gmail.com>
2023-10-04 15:50:41 +0500

    Customer Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_104252) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_105003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_104252) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "total_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
