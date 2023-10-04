5740e7ebe0de34d367f474afa554440a0021b08a
ZuhaaRana <zuhaarana@gmail.com>
2023-10-04 12:33:05 +0500

    Cart Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_04_071050) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_04_073242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_04_071050) do
     t.string "name"
     t.integer "quantity"
     t.string "unit_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "total_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
