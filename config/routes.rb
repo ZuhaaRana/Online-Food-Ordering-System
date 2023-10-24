3e5ca836a259d0af93ffcc040d2663efe7d916bc
ZuhaaRana <zuhaarana@gmail.com>
2023-10-24 13:36:30 +0500

    Integrated client side template

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   resources :order_order_details
   resources :menu_item_carts
   resources :menu_item_addons
-  get 'welcome/index'
-  root 'dashboard#display'
+  root 'welcome#index'
+  get 'dashboard/display'
+  get '/search', to: 'search#search', as: 'search'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
